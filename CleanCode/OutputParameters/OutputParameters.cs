e2a22ee76ae9cffcbd5df45af95854b56f27a0db
jame2408 <jame2408@gmail.com>
2017-03-04 19:41:40 +0800

    Initial OutputParameters

---
+++ CleanCode/OutputParameters/OutputParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.OutputParameters
+{
+    public class OutputParameters
+    {
+        public void DisplayCustomers()
+        {
+            int totalCount = 0;
+            var customers = GetCustomers(1, out totalCount);
+
+            Console.WriteLine("Total customers: " + totalCount);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+        }
+
+        private IEnumerable<Customer> GetCustomers(int pageIndex, out int totalCount)
+        {
+            totalCount = 100;
+            return new List<Customer>();
+        }
+    }
+
+    public class Customer
+    {
+        public string name { get; set; }
+        public string age { get; set; }
+        public string phone { get; set; }
+    }
+
+
+}
